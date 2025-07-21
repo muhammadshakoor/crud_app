064d9ed714c32f4299bdbdf583e95ce709e9aeeb
Muhammad Shakoor <muhammadshakoor86@gmail.com>
2025-07-21 18:11:21 +0500

    api istead of url

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  // baseURL: 'http://localhost:8080/api',
+  baseURL: '/api',
 });
 
 api.interceptors.request.use(config => {
